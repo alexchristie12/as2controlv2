c885a361e9df4d90e08c0c8baf558c8a6244254b
Alex Christie <achris22@protonmail.com>
2024-10-20 12:06:47 +1000

    add watering cancelling endpoint

--- control/control_handlers.go
+++ control/control_handlers.go
@@ -46,3 +46,33 @@ func (cs *ControlSystem) RoutePOSTDelayWatering(c *gin.Context) {
 		"msg": fmt.Sprintf("unit %d has been delayed by 60 minutes", unit),
 	})
 }
+
+func (cs *ControlSystem) RoutePOSTCancelWatering(c *gin.Context) {
+	unitStr := c.Query("unit")
+	if unitStr == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "no unit to stop watering given",
+		})
+	}
+
+	unit, err := strconv.Atoi(unitStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "invalid unit to stop, parse error",
+		})
+	}
+
+	if _, ok := cs.systemTiming.WateringUntilTime[uint(unit)]; !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "unit is not watering",
+		})
+	}
+
+	// Otherwise cancel the thing
+	if err = cs.HandleWateringOffEvent(uint(unit)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": fmt.Sprintf("could not switch off watering for unit %d", unit),
+		})
+	}
+
+}
